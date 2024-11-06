4a192a2f9ac2ca9985400a3c6ef23acc87fc1a2e
Ramirez474747 <114956756+fernando45587652@users.noreply.github.com>
2024-11-06 14:33:27 -0600

    Add files via upload

---
+++ create.blade.php
@@ -0,0 +1,8 @@
+Seccion para crear pais
+<form action="{{url('/pais')}}" method="pod" enctype="multipart/form-data">
+
+<label for="Nombre">{{'Nombre'}}</label>    
+<input type="text" name="Nombre" id="Nombre" value="">
+<input type="submit" value="Agregar">
+
+</form>
