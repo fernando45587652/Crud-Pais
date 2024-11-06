dcb143de2f5cc8defdfb74f3dda6aa018e2f238e
Ramirez474747 <114956756+fernando45587652@users.noreply.github.com>
2024-11-06 14:26:10 -0600

    Update index.blade.php

--- index.blade.php
+++ index.blade.php
@@ -1,1 +1,25 @@
 Inicio(Despliegue de Datos)
+
+<table class="table table-light">
+
+    <thead class="thead-light">
+        <tr>
+            <th>#</th>
+            <th>idcontry</th>
+            <th>contryname</th>
+            <th>contry_e</th>
+           <th>contry_idmh</th>
+          
+            
+        </tr>
+    </thead>
+    
+    <tbody>
+        @foreach ( $contry as $contry)
+tr>
+            <td>{{$loop->iteration}} </td>
+            
+        </tr>
+        @endforeach
+    </tbody>
+</table>
